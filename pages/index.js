d7665e93c90dd5e0e338b5e636f0ad035528a901
Daniel Kim <danieljoonkimm@gmail.com>
2020-08-14 20:52:43 -0700

    [edit] fixing

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,48 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {InputN} from './inputN'
-import {MakeBoard} from './makeBoard'
+import styled from 'styled-components'
 
-const initialNumber = 8
+const StyleDiv = styled.div`
+ display: grid;
+ grid-template-columns: repeat(${props => props.length}, 1fr);
+
+`
+
+
 export default function Home() {
-  const [number, setNumber] = useState(initialNumber)
+  let num = 4
+  const [newBoard, setNewBoard] = useState()
+  const [initial, setInitial] = useState(num)
+  const [flag, setFlag] = useState(false)
+  useEffect(() => {
+    let boardData = createBoard()
+    
+    setNewBoard(boardData)
+  }, [flag, initial])
   
-  const onNumberChange = (num) => {
-    setNumber(num)
-  }
+  const createBoard = () => {
+    var matrix = [];
+    for(var i=0; i< initial; i++) {
+        matrix[i] = [];
+        for(var j=0; j< initial; j++) {
+        matrix[i][j] = <div style={{'backgroundColor': (j%2 === 0 && i % 2 === 0) ? 'black' : (j%2 !== 0 && i% 2 !==0) ? 'black' : 'red'}}
+        >{j}
+        </div>;
+        }
+    }
 
-  return (
-    <div className={styles.container}>
-      <InputN onNumberChange={onNumberChange}/>
-      <MakeBoard number={number} setNumber={setNumber}/>
-    </div>
-  )
+    return matrix
+  }
+  
+    return (
+      <div className="App">
+        <form onSubmit={e => e.preventDefault(), () => setFlag(c => !c)}>
+  
+        <input type='number' onChange={e => setInitial(e.target.value)}></input>
+        <button type='submit'></button>
+        </form>
+        {newBoard === undefined ? 'loading' : <div>{newBoard.map((arr, index) => <StyleDiv index={index} length={arr.length}>{arr}</StyleDiv>)}</div>}
+      </div>
+    );
 }
